f4a828c66237611170fb61f2b16e29dd4d82281c
Snewar79 <himera5x@yandex.ru>
2018-05-14 19:17:15 +0400

    Somthing is work

---
+++ RayTracing/RayTracing/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace RayTracing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(37, 55);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(675, 321);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseMove);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 439);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
+
---
+++ RayTracing/RayTracing/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace RayTracing
+{
+    public partial class Form1 : Form
+    {
+        Graphics gr;
+        RayTracing scene;
+        public Form1()
+        {
+            InitializeComponent();
+            gr = new Graphics();
+            scene = new RayTracing();
+        }
+
+        
+private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            // gr.Update();
+            scene.Update();
+
+            glControl1.SwapBuffers();
+            //gr.closeProgram();
+            scene.closeProgram();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            scene.Resize(glControl1.Width, glControl1.Height);
+            Application.Idle += Application_Idle;
+            //gr.Resize(glControl1.Width, glControl1.Height);
+        }
+
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            while (glControl1.IsIdle)
+                glControl1.Refresh();
+        }
+
+        private void glControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            float heightCoef = (-e.Y + glControl1.Height) / (float)glControl1.Height;
+            float  widthCoef = (e.X - glControl1.Width * 0.5f) / (float)glControl1.Width;
+            scene.latitude = widthCoef * 360; // Широта
+            scene.longitude = heightCoef * 90; //Долгота
+        }
+    }
+}
